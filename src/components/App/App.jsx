92057b9c38f92fe12cea419d55913cdb956a4784
pentinmaki <keijo.pentinmaki@gmail.com>
2025-01-21 00:03:24 +0200

    lisää AppRouter-komponentin ja reitityksen

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,26 +1,10 @@
-import styles from './App.module.scss'
-import Header from '../Header'
-import Menu from '../Menu'
-import Content from '../Content'
-import { FloatingButton } from '../../shared/buttons'
-import { ButtonContainer } from '../../shared/buttons'
-import Items from '../Items'
-import Stats from '../Stats'
-import Settings from '../Settings'
+import AppRouter from '../AppRouter'
 
 function App() {
 
-    return (
+  return (
     <>
-      <ButtonContainer>
-      <div className={styles.app}>
-        <Header />
-        <Content>
-          <Settings />
-        </Content>
-        <Menu />
-      </div>
-      </ButtonContainer>
+      <AppRouter />
     </>
   )
 }
