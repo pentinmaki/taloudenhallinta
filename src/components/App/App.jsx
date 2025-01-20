e2d4c44e4086f5276ef3af99a96f01e988b4e34d
pentinmaki <keijo.pentinmaki@gmail.com>
2025-01-20 23:35:42 +0200

    lisää FloatingButton-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,7 @@ import Item from '../Item'
 import Header from '../Header'
 import Menu from '../Menu'
 import Content from '../Content'
-import Button from '../../shared/buttons'
+import { FloatingButton } from '../../shared/buttons'
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
           <Item />
           <Item />
           <Item />
-          <Button primary>LISÄÄ UUSI RIVI</Button>
+          <FloatingButton secondary>+</FloatingButton>
         </Content>     
         <Menu />
       </div>
