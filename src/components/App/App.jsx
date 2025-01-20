add819a62369f782bf366a55a76bffa832c7cda1
pentinmaki <keijo.pentinmaki@gmail.com>
2025-01-20 23:47:07 +0200

    lisää Stats-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Content from '../Content'
 import { FloatingButton } from '../../shared/buttons'
 import { ButtonContainer } from '../../shared/buttons'
 import Items from '../Items'
+import Stats from '../Stats'
 
 function App() {
 
@@ -14,7 +15,7 @@ function App() {
       <div className={styles.app}>
         <Header />
         <Content>
-          <Items />
+          <Stats />
         </Content>
         <Menu />
       </div>
