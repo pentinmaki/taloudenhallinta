4c2b3866e07730d1d2008d6845ddde76fc82bb10
pentinmaki <keijo.pentinmaki@gmail.com>
2025-01-20 22:47:08 +0200

    lisää Header-komponentin

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import styles from './App.module.scss'
 import Item from '../Item'
+import Header from '../Header'
 
 function App() {
 
   return (
     <>
       <div className={styles.app}>
+        <Header />
         <Item />
         <Item />
         <Item />
